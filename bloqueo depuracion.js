0829b9822a8ebe1df14ffba48522bca55591999f
Nat7614 <vnatamata357@gmail.com>
2025-05-01 15:31:39 +0200

    Delete bloqueo depuracion.js

--- bloqueo depuracion.js
+++
@@ -1,8 +0,0 @@
-setInterval(() => {
-    (function() {
-        if (window.console && (console.firebug || console.table && /firebug/i.test(console.table()))) {
-            alert("Depuración detectada. Se cerrará la aplicación.");
-            window.close();
-        }
-    })();
-}, 1000);
