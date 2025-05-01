bd823cd5f5746abe3ef966e3af13224a742baeab
Nat7614 <vnatamata357@gmail.com>
2025-05-01 15:31:52 +0200

    Delete bloqueo.js

--- bloqueo.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("contextmenu", event => event.preventDefault()); // Bloquea clic derecho
-
-document.addEventListener("keydown", event => {
-    if (event.ctrlKey && (event.key === "u" || event.key === "U")) { 
-        event.preventDefault(); // Bloquea Ctrl + U (Ver código fuente)
-    }
-});
